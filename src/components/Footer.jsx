b9e8e171f9653df453ed9f7be370bda58e5d6e23
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-08-15 19:26:42 +0530

    Routing Practices are Much needed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <>
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright{" "}
+            <Link to={"mailTo:nauticalvigneshmenon@gmail.com"}>
+              ©nauticalvigneshmenon@gmail.com
+            </Link>{" "}
+            2024
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
